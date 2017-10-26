a06de3818304ffba029c643e2600e42acbd637f7
ramonob13 <33110440+ramonob13@users.noreply.github.com>
2017-10-26 13:19:40 -0300

    BrincandoComArray2.java

---
+++ Array/src/br/edu/ifpb/esperanca/poo/array/BrincandoComArray2.java
@@ -0,0 +1,19 @@
+package br.edu.ifpb.esperanca.poo.array;
+//teste alterando
+import java.util.ArrayList;
+
+public class BrincandoComArray2 {
+
+	public static void main(String[] args) {
+		ArrayList lista = new ArrayList();
+		lista.add(10);
+		lista.add(6.9);
+		lista.add("a");
+		for (int i =0; i < lista.size();i++) {
+			System.out.println(lista.get(i));
+		}
+		
+		
+	}
+	
+}
